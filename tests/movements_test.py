e6f4de13810dc609c0547981b3a7a1e250a867fd
Guillaume Le Pape <gui.lepape25@gmail.com>
2024-12-29 23:08:12 +0100

    Use pytest parametrize

--- tests/movements_test.py
+++ tests/movements_test.py
@@ -1,60 +1,47 @@
+import pytest
+
 from rubik import Movement, parse_movements
 
 
-def test_movements_perm_nb() -> None:
-    movements = parse_movements("(R' U R U' R') (F' U' F) (R U R') (F R' F') (R U' R)")
-
-    assert movements == [
-        (Movement.R_PRIME, 1),
-        (Movement.U, 1),
-        (Movement.R, 1),
-        (Movement.U_PRIME, 1),
-        (Movement.R_PRIME, 1),
-        (Movement.F_PRIME, 1),
-        (Movement.U_PRIME, 1),
-        (Movement.F, 1),
-        (Movement.R, 1),
-        (Movement.U, 1),
-        (Movement.R_PRIME, 1),
-        (Movement.F, 1),
-        (Movement.R_PRIME, 1),
-        (Movement.F_PRIME, 1),
-        (Movement.R, 1),
-        (Movement.U_PRIME, 1),
-        (Movement.R, 1),
-    ]
-
-
-def test_movements_perm_h() -> None:
-    movements = parse_movements("M2' U M2' U2 M2' U M2'")
-
-    assert movements == [
+@pytest.mark.parametrize(
+    ("raw_movements", "parsed_movements"),
+    [
         (
-            Movement.M_PRIME,
-            2,
+            "(R' U R U' R') (F' U' F) (R U R') (F R' F') (R U' R)",
+            [
+                (Movement.R_PRIME, 1),
+                (Movement.U, 1),
+                (Movement.R, 1),
+                (Movement.U_PRIME, 1),
+                (Movement.R_PRIME, 1),
+                (Movement.F_PRIME, 1),
+                (Movement.U_PRIME, 1),
+                (Movement.F, 1),
+                (Movement.R, 1),
+                (Movement.U, 1),
+                (Movement.R_PRIME, 1),
+                (Movement.F, 1),
+                (Movement.R_PRIME, 1),
+                (Movement.F_PRIME, 1),
+                (Movement.R, 1),
+                (Movement.U_PRIME, 1),
+                (Movement.R, 1),
+            ],
         ),
         (
-            Movement.U,
-            1,
+            "M2' U M2' U2 M2' U M2'",
+            [
+                (Movement.M_PRIME, 2),
+                (Movement.U, 1),
+                (Movement.M_PRIME, 2),
+                (Movement.U, 2),
+                (Movement.M_PRIME, 2),
+                (Movement.U, 1),
+                (Movement.M_PRIME, 2),
+            ],
         ),
-        (
-            Movement.M_PRIME,
-            2,
-        ),
-        (
-            Movement.U,
-            2,
-        ),
-        (
-            Movement.M_PRIME,
-            2,
-        ),
-        (
-            Movement.U,
-            1,
-        ),
-        (
-            Movement.M_PRIME,
-            2,
-        ),
-    ]
+    ],
+    ids=("perm nb", "perm h"),
+)
+def test_movements(raw_movements: str, parsed_movements: list[tuple[Movement, int]]) -> None:
+    assert parsed_movements == parse_movements(raw_movements)
--- tests/unchanged_test.py
+++ tests/unchanged_test.py
@@ -1,158 +1,53 @@
-from rubik import Movement, Rubik, parse_movements
+import pytest
+
+from rubik import Rubik, parse_movements
 
 
-def test_six_sexy_moves() -> None:
+@pytest.mark.parametrize(
+    ("number_of_cycles", "algorithms", "auf"),
+    [
+        (6, "R U R' U'", None),
+        (1, "U U'", None),
+        (1, "U U U U", None),
+        (1, "R4", None),
+        (3, "R U R' U R' U' R2 U' R' U R' U R", "U2"),
+        (3, "M2' U M U2 M' U M2'", None),
+        (3, "R' U R' U' R3 U' R' U R U R2", None),
+        (2, "R U R' U R U R' F' R U R' U' R' F R2 U' R' U2 R U' R'", None),
+        (2, "(R' U R U' R') (F' U' F) (R U R') (F R' F') (R U' R)", None),
+        (2, "L U' R' U L' U2 R U' R' U2 R", None),
+        (2, "R U R' F' R U R' U' R' F R2 U' R' U'", None),
+        (2, "M2' U M2' U2 M2' U M2'", None),
+    ],
+    ids=(
+        "sexy moves",
+        "up face",
+        "4 up moves",
+        "4 right moves",
+        "perm ua",
+        "perm ua alt",
+        "perm ub",
+        "perm na",
+        "perm nb",
+        "perm ja",
+        "perm jb",
+        "perm h",
+    ),
+)
+def test_algorithms_cycle(number_of_cycles: int, algorithms: str, auf: str | None) -> None:
     rubik = Rubik()
 
     assert rubik.is_solved()
 
-    for _ in range(6):
-        rubik.move(Movement.R)
-        rubik.move(Movement.U)
-        rubik.move(Movement.R_PRIME)
-        rubik.move(Movement.U_PRIME)
+    movements = parse_movements(algorithms)
+    auf_movements = parse_movements(auf) if auf is not None else None
 
-    assert rubik.is_solved()
-
-
-def test_up_face() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    rubik.move(Movement.U)
-    rubik.move(Movement.U_PRIME)
-
-    assert rubik.is_solved()
-
-
-def test_4_up_moves() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    rubik.move(Movement.U)
-    rubik.move(Movement.U)
-    rubik.move(Movement.U)
-    rubik.move(Movement.U)
-
-    assert rubik.is_solved()
-
-    rubik.move(Movement.R, 4)
-
-    assert rubik.is_solved()
-
-
-def test_perm_ua() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    # perform perm ua 3 times, cube should be back to its initial position
-    movements = parse_movements("R U R' U R' U' R2 U' R' U R' U R")
-
-    for _ in range(3):
+    for _ in range(number_of_cycles):
         for movement, number_of_moves in movements:
             rubik.move(movement, number_of_moves)
 
-        rubik.move(Movement.U, 2)
-
-    assert rubik.is_solved()
-
-
-def test_perm_ua_alternative_algorithm() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    movements = parse_movements("M2' U M U2 M' U M2'")
-
-    for _ in range(3):
-        for movement, number_of_moves in movements:
-            rubik.move(movement, number_of_moves)
-
-    assert rubik.is_solved()
-
-
-def test_perm_ub() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    movements = parse_movements("R' U R' U' R3 U' R' U R U R2")
-
-    for _ in range(3):
-        for movement, number_of_moves in movements:
-            rubik.move(movement, number_of_moves)
-
-    assert rubik.is_solved()
-
-
-def test_perm_na() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    movements = parse_movements("R U R' U R U R' F' R U R' U' R' F R2 U' R' U2 R U' R'")
-
-    for _ in range(2):
-        for movement, number_of_moves in movements:
-            rubik.move(movement, number_of_moves)
-
-    assert rubik.is_solved()
-
-
-def test_perm_nb() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    movements = parse_movements("(R' U R U' R') (F' U' F) (R U R') (F R' F') (R U' R)")
-
-    for _ in range(2):
-        for movement, number_of_moves in movements:
-            rubik.move(movement, number_of_moves)
-
-    assert rubik.is_solved()
-
-
-def test_perm_ja() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    movements = parse_movements("L U' R' U L' U2 R U' R' U2 R")
-
-    for _ in range(2):
-        for movement, number_of_moves in movements:
-            rubik.move(movement, number_of_moves)
-
-    assert rubik.is_solved()
-
-
-def test_perm_h() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    movements = parse_movements("M2' U M2' U2 M2' U M2'")
-
-    for _ in range(2):
-        for movement, number_of_moves in movements:
-            rubik.move(movement, number_of_moves)
-
-    assert rubik.is_solved()
-
-
-def test_perm_dddd() -> None:
-    rubik = Rubik()
-
-    assert rubik.is_solved()
-
-    movements = parse_movements("M2' U M U2 M' U M2'")
-
-    for _ in range(3):
-        for movement, number_of_moves in movements:
-            rubik.move(movement, number_of_moves)
+        if auf is not None:
+            for movement, number_of_moves in auf_movements:
+                rubik.move(movement, number_of_moves)
 
     assert rubik.is_solved()
