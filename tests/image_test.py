2451a31ab5e1493a75bfe59b50a0cbc10d39a83a
Guillaume Le Pape <gui.lepape25@gmail.com>
2025-09-06 17:43:27 +0200

    Download menu icons

--- tests/image_test.py
+++ tests/image_test.py
@@ -6,11 +6,14 @@ ROOT_DIR = Path(__file__).parents[1]
 
 
 @pytest.mark.parametrize(
-    ("folder", "file_extension", "count"),
+    ("folder", "file_prefix", "file_extension", "count"),
     [
-        ("oll", "png", 57),
-        ("pll", "webp", 21),
+        ("oll", "O", "png", 57),
+        ("pll", "", "webp", 21),
     ],
 )
-def test_images(folder: str, file_extension: str, count: int) -> None:
-    assert len(list((ROOT_DIR / "docs" / "images" / folder).glob(f"*.{file_extension}"))) == count
+def test_images(folder: str, file_prefix: str, file_extension: str, count: int) -> None:
+    assert (
+        len(list((ROOT_DIR / "docs" / "images" / folder).glob(f"{file_prefix}*.{file_extension}")))
+        == count
+    )
