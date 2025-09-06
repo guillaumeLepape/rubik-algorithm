ef5abdd1bb16a38977e4d2b745dd606e490b492f
Guillaume Le Pape <gui.lepape25@gmail.com>
2025-09-06 14:38:35 +0200

    Add the last OLL algorithms

--- macros.py
+++ macros.py
@@ -7,6 +7,10 @@ IMAGE_WIDTH = 120
 
 def define_env(env) -> None:
     @env.macro
+    def img_f2l(number: int) -> str:
+        return render_image("f2l", f"F{number}.png", IMAGE_WIDTH)
+
+    @env.macro
     def img_oll(number: int) -> str:
         return render_image("oll", f"O{number}.png", IMAGE_WIDTH)
 
