484c7fbf6d09d118157c6c315fed1305bf3529ea
Guillaume Le Pape <gui.lepape25@gmail.com>
2025-09-07 18:55:46 +0200

    Separate cube rotation from face rotation

--- tests/movements_test.py
+++ tests/movements_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from rubik_algorithm import Movement, parse_movements
+from rubik_algorithm import FaceRotation, parse_movements
 
 
 @pytest.mark.parametrize(
@@ -9,39 +9,39 @@ from rubik_algorithm import Movement, parse_movements
         (
             "(R' U R U' R') (F' U' F) (R U R') (F R' F') (R U' R)",
             [
-                (Movement.R_PRIME, 1),
-                (Movement.U, 1),
-                (Movement.R, 1),
-                (Movement.U_PRIME, 1),
-                (Movement.R_PRIME, 1),
-                (Movement.F_PRIME, 1),
-                (Movement.U_PRIME, 1),
-                (Movement.F, 1),
-                (Movement.R, 1),
-                (Movement.U, 1),
-                (Movement.R_PRIME, 1),
-                (Movement.F, 1),
-                (Movement.R_PRIME, 1),
-                (Movement.F_PRIME, 1),
-                (Movement.R, 1),
-                (Movement.U_PRIME, 1),
-                (Movement.R, 1),
+                (FaceRotation.R_PRIME, 1),
+                (FaceRotation.U, 1),
+                (FaceRotation.R, 1),
+                (FaceRotation.U_PRIME, 1),
+                (FaceRotation.R_PRIME, 1),
+                (FaceRotation.F_PRIME, 1),
+                (FaceRotation.U_PRIME, 1),
+                (FaceRotation.F, 1),
+                (FaceRotation.R, 1),
+                (FaceRotation.U, 1),
+                (FaceRotation.R_PRIME, 1),
+                (FaceRotation.F, 1),
+                (FaceRotation.R_PRIME, 1),
+                (FaceRotation.F_PRIME, 1),
+                (FaceRotation.R, 1),
+                (FaceRotation.U_PRIME, 1),
+                (FaceRotation.R, 1),
             ],
         ),
         (
             "M2' U M2' U2 M2' U M2'",
             [
-                (Movement.M_PRIME, 2),
-                (Movement.U, 1),
-                (Movement.M_PRIME, 2),
-                (Movement.U, 2),
-                (Movement.M_PRIME, 2),
-                (Movement.U, 1),
-                (Movement.M_PRIME, 2),
+                (FaceRotation.M_PRIME, 2),
+                (FaceRotation.U, 1),
+                (FaceRotation.M_PRIME, 2),
+                (FaceRotation.U, 2),
+                (FaceRotation.M_PRIME, 2),
+                (FaceRotation.U, 1),
+                (FaceRotation.M_PRIME, 2),
             ],
         ),
     ],
     ids=("perm nb", "perm h"),
 )
-def test_movements(raw_movements: str, parsed_movements: list[tuple[Movement, int]]) -> None:
+def test_movements(raw_movements: str, parsed_movements: list[tuple[FaceRotation, int]]) -> None:
     assert parsed_movements == parse_movements(raw_movements)
